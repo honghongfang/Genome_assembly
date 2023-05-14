f3da2281de091d72b7b339f6ad12856bc305a193
Honghong Fang <93330779+honghongfang@users.noreply.github.com>
2023-05-14 16:24:44 +0200

    Add files via upload

---
+++ 01_3_read_statistics.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=4000M
+#SBATCH --time=01:00:00
+#SBATCH --job-name=fastqc
+#SBATCH --mail-user=honghong.fang@students.unibe.ch
+#SBATCH --mail-type=begin,end
+#SBATCH --output=/data/users/hfang/newassembly/read_QC/fastqc/pacbio/output_fastqc_%j.o
+#SBATCH --error=/data/users/hfang/newassembly/read_QC/fastqc/pacbio/error_fastqc_%j.e
+#SBATCH --partition=pall
+
+
+module add UHTS/Quality_control/fastqc/0.11.9
+
+INPUT_FILES=/data/users/hfang/newassembly/participant_1/pacbio
+
+OUTPUT_DIR=/data/users/hfang/newassembly/read_QC/fastqc/pacbio
+
+fastqc --extract $INPUT_FILES/*.fastq.gz --threads 1 -o $OUTPUT_DIR
